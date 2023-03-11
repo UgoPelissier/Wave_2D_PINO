5f9576a7108c9971a0ceadf78dc5936331667033
ugo.pelissier <ugo.pelissier@etu.minesparis.psl.eu>
2023-03-11 18:28:48 +0000

    PINO

--- dataset/data.py
+++ dataset/data.py
@@ -7,13 +7,29 @@ Created on Thu Mar  9 11:32:01 2023
 
 import torch
 
-from .utils.random_field import GRF_Mattern
-from .utils.wave_2D import WaveEq2D
+from utils.random_field import GRF_Mattern
+from utils.wave_2D import WaveEq2D
 from functorch import vmap
-from .utils.load import load_config
-from .utils.loader import DataLoader2D
+from utils.load import load_config
+from utils.loader import DataLoader2D
 
-def data_generator(dim, N, L, Nu, l, sigma, device, Nsamples, Nx, Ny, dt, save_int):
+import modulus
+from modulus.hydra import ModulusConfig
+
+dim = 2
+N = 128
+Nx = 128
+Ny = 128
+l = 0.1
+L = 1.0
+sigma = 1.0
+Nu = None
+Nsamples = 2
+dt = 1.0e-4
+save_int = int(1e-2/dt)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+def data_generator(dim, N, L, Nu, l, sigma, device, Nsamples, Nx, Ny, dt, save_int, phase, i):
     grf = GRF_Mattern(dim, N, length=L, nu=Nu, l=l, sigma=sigma, boundary="periodic", device=device)
     U0 = grf.sample(Nsamples)
     u0 = U0.cpu().float()
@@ -25,4 +41,50 @@ def data_generator(dim, N, L, Nu, l, sigma, device, Nsamples, Nx, Ny, dt, save_i
     u = U.cpu().float()
     outvar = {'sol': u}
     
-    return invar, outvar
+    torch.save(invar, 'invar_'+phase+'_'+str(i)+'.pt')
+    torch.save(outvar, 'outvar_'+phase+'_'+str(i)+'.pt')
+
+@modulus.main(config_path="../conf", config_name="config_PINO")
+def run(cfg: ModulusConfig) -> None:
+    for i in range(10):
+        data_generator(dim, N, L, Nu, l, sigma, device, cfg['custom']['ntrain'], Nx, Ny, dt, save_int, 'train', i)
+        data_generator(dim, N, L, Nu, l, sigma, device, cfg['custom']['ntest'], Nx, Ny, dt, save_int, 'test', i)
+        
+    invar_train = torch.load("invar_train_0.pt")
+    invar_train["IC"].shape
+    for i in range(1,10):
+        name = "invar_train_" + str(i) + ".pt"
+        temp = torch.load(name)
+        invar_train["IC"] = torch.cat((invar_train["IC"], temp["IC"]), 0)
+        print(invar_train["IC"].shape)
+    torch.save(invar_train, 'invar_train.pt')
+    
+    outvar_train = torch.load("outvar_train_0.pt")
+    outvar_train["sol"].shape
+    for i in range(1,10):
+        name = "outvar_train_" + str(i) + ".pt"
+        temp = torch.load(name)
+        outvar_train["sol"] = torch.cat((outvar_train["sol"], temp["sol"]), 0)
+        print(outvar_train["sol"].shape)
+    torch.save(outvar_train, 'outvar_train.pt')
+    
+    invar_test = torch.load("invar_test_0.pt")
+    invar_test["IC"].shape
+    for i in range(1,10):
+        name = "invar_test_" + str(i) + ".pt"
+        temp = torch.load(name)
+        invar_test["IC"] = torch.cat((invar_test["IC"], temp["IC"]), 0)
+        print(invar_test["IC"].shape)
+    torch.save(invar_test, 'invar_test.pt')
+    
+    outvar_test = torch.load("outvar_test_0.pt")
+    outvar_test["sol"].shape
+    for i in range(1,10):
+        name = "outvar_test_" + str(i) + ".pt"
+        temp = torch.load(name)
+        outvar_test["sol"] = torch.cat((outvar_test["sol"], temp["sol"]), 0)
+        print(outvar_test["sol"].shape)
+    torch.save(outvar_test, 'outvar_test.pt')
+    
+if __name__ == "__main__":
+    run()
---
+++ wave_2d_PINO.py
@@ -0,0 +1,159 @@
+from typing import Dict
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+import modulus
+from modulus.hydra import instantiate_arch, ModulusConfig
+from modulus.key import Key
+from modulus.models.layers.spectral_layers import fourier_derivatives
+from modulus.node import Node
+
+from modulus.solver import Solver
+from modulus.domain import Domain
+from modulus.domain.constraint import SupervisedGridConstraint
+from modulus.domain.validator import GridValidator
+from modulus.dataset import DictGridDataset
+from modulus.utils.io.plotter import GridValidatorPlotter
+from modulus.utils.io.vtk import grid_to_vtk
+
+from utilities import download_FNO_dataset, load_FNO_dataset
+from ops import dx, ddx
+
+dim = 2
+N = 128
+Nx = 128
+Ny = 128
+l = 0.1
+L = 1.0
+sigma = 1.0
+Nu = None
+dt = 1.0e-4
+save_int = int(1e-2/dt)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+class Wave(torch.nn.Module):
+    "Custom Wave PDE definition for PINO"
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input_var: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # get inputs
+        u = input_var["sol"]
+        ic = input_var["IC"]
+        c = 1.0
+        
+        dxf = 1.0 / u.shape[-2]
+        dyf = 1.0 / u.shape[-1]
+        
+        dduddx_exact = input_var["sol__x__x"]
+        dduddy_exact = input_var["sol__y__y"]
+        # compute wave equation
+        wave = (
+            1.0
+            + (c * dduddx_exact)
+            + (c * dduddy_exact)
+        )
+
+        # Zero outer boundary
+        wave = F.pad(wave[:, :, 2:-2, 2:-2], [2, 2, 2, 2], "constant", 0)
+        # Return darcy
+        output_var = {
+            "wave": dxf * wave,
+        }  # weight boundary loss higher
+        return output_var
+    
+@modulus.main(config_path="conf", config_name="config_PINO")
+def run(cfg: ModulusConfig) -> None:
+    
+    # [datasets]
+    # load training/ test data
+    invar_train = torch.load('dataset/invar_train.pt')
+    outvar_train = torch.load('dataset/outvar_train.pt')
+    invar_test = torch.load('dataset/invar_test.pt')
+    outvar_test = torch.load('dataset/outvar_test.pt')
+    
+    input_keys = [
+        Key("IC", scale=(torch.mean(invar_train['IC']), torch.std(invar_train['IC'])))
+    ]
+    output_keys = [
+        Key("sol", scale=(torch.mean(outvar_train['sol']), torch.std(outvar_train['sol'])))
+    ]
+    
+    outvar_train['sol'] = outvar_train['sol'][:,100:,:,:]
+    outvar_test['sol'] = outvar_test['sol'][:,100:,:,:]
+    print('IC shape: ', outvar_train['sol'].shape)
+
+    train_dataset = DictGridDataset(invar_train, outvar_train)
+    test_dataset = DictGridDataset(invar_test, outvar_test)
+    # [datasets]
+
+    # [init-model]
+    # Define FNO model
+    decoder_net = instantiate_arch(
+            cfg=cfg.arch.decoder,
+            output_keys=output_keys,
+        )
+    fno = instantiate_arch(
+            cfg=cfg.arch.fno,
+            input_keys=[input_keys[0]],
+            decoder_net=decoder_net,
+        )
+    derivatives = [
+        Key("sol", derivatives=[Key("x"), Key("x")]),
+        Key("sol", derivatives=[Key("y"), Key("y")]),
+    ]
+    # fno.add_pino_gradients(
+    #     derivatives=derivatives,
+    #     domain_length=[1.0, 1.0],
+    # )
+    # [init-model]
+
+    # [init-node]
+    # Make custom Darcy residual node for PINO
+    inputs = [
+        "sol",
+        "IC"
+    ]
+    wave_node = Node(
+        inputs=inputs,
+        outputs=["wave"],
+        evaluate=Wave(),
+        name="Wave Node",
+    )
+    nodes = [fno.make_node('fno'), wave_node]
+    # [init-node]
+
+    # [constraint]
+    # make domain
+    domain = Domain()
+
+    # add constraints to domain
+    supervised = SupervisedGridConstraint(
+        nodes=nodes,
+        dataset=train_dataset,
+        batch_size=cfg.batch_size.grid,
+    )
+    domain.add_constraint(supervised, "supervised")
+    # [constraint]
+
+    # add validator
+    val = GridValidator(
+        nodes,
+        dataset=test_dataset,
+        batch_size=cfg.batch_size.validation,
+        plotter=GridValidatorPlotter(n_examples=5),
+        requires_grad=True,
+    )
+    domain.add_validator(val, "test")
+
+    # make solver
+    slv = Solver(cfg, domain)
+
+    # start solver
+    slv.solve()
+
+if __name__ == "__main__":
+    run()
