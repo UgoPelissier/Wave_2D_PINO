09a02b0065e8caaa8ed3b0b1e12aa3edd8e35507
ugo.pelissier <ugo.pelissier@etu.minesparis.psl.eu>
2023-03-13 16:30:56 +0000

    Report

--- wave_2d_PINO.py
+++ wave_2d_PINO.py
@@ -18,8 +18,8 @@ from modulus.dataset import DictGridDataset
 from modulus.utils.io.plotter import GridValidatorPlotter
 from modulus.utils.io.vtk import grid_to_vtk
 
-from utilities import download_FNO_dataset, load_FNO_dataset
-from ops import dx, ddx
+#from utilities import download_FNO_dataset, load_FNO_dataset
+#from ops import dx, ddx
 
 class Wave(torch.nn.Module):
     "Custom Wave PDE definition for PINO"
